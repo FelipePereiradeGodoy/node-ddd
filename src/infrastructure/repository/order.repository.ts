f4dea336faee7dd44b7238d481813bf707d94524
FelipePereiradeGodoy <godoypereirafelipe@gmail.com>
2025-02-05 11:32:34 -0300

    config inicial

---
+++ src/infrastructure/repository/order.repository.ts
@@ -0,0 +1,86 @@
+import Order from "../../domain/entity/order";
+import OrderItem from "../../domain/entity/order-item";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderModel from "../db/sequelize/model/order.model";
+
+
+export default class OrderRepository implements OrderRepositoryInterface {
+  async create(entity: Order): Promise<void> {
+    await OrderModel.create(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        include: [{ model: OrderItemModel }],
+      }
+    );
+  }
+
+  async update(entity: Order): Promise<void> {
+    for (const item of entity.items) {
+      await OrderItemModel.update(
+        {
+          quantity: item.quantity,
+        },
+        {
+          where: { id: item.id },
+        },
+      );
+    }
+
+    await OrderModel.update(
+      {
+        total: entity.total(),
+      },
+      {
+        where: {
+            id: entity.id,
+        },
+      },
+    );
+  }
+
+  async find(id: string): Promise<Order> {
+    const order = await OrderModel.findOne({
+      where: { id },
+      include: [{ model: OrderItemModel, as: "items" }],
+    });
+  
+    return new Order(
+      order.id,
+      order.customer_id,
+      order.items.map(
+        (item) =>
+          new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity)
+      )
+    );
+  }
+
+  async findAll(): Promise<Order[]> {
+    const orders = await OrderModel.findAll({
+      include: [{ model: OrderItemModel, as: "items" }],
+    });
+  
+    return orders.map(
+      (order) =>
+        new Order(
+          order.id,
+          order.customer_id,
+          order.items.map(
+            (item) =>
+              new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity)
+          )
+        )
+    );
+  }
+}
