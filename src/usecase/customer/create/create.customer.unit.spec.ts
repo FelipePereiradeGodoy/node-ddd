beb20cc483229b9bb7a566f0f81f7adff6e592cc
FelipePereiradeGodoy <godoypereirafelipe@gmail.com>
2025-04-23 21:04:10 -0300

    feat: implement create customer use case and DTOs

---
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,54 @@
+import CreateCustomerUseCase from "./create.customer.usecase";
+
+const input = {
+    name: 'John',
+    address: {
+        street: 'Street',
+        number: 123,
+        zip: 'Zip',
+        city: 'City',
+    },
+};
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    };
+}
+
+describe('Unit Test create customer use case', () => {
+    it('should create a customer', async () => {
+        const customerRepository = MockRepository();
+        const usecase = new CreateCustomerUseCase(customerRepository);
+
+        const output = await usecase.execute(input);
+
+        const expectedOutput = {
+            id: expect.any(String),
+            name: input.name,
+            address: {
+                street: input.address.street,
+                number: input.address.number,
+                zip: input.address.zip,
+                city: input.address.city,
+            },
+        }
+
+        expect(output).toEqual(expectedOutput);
+    });
+
+    it('should throw an error when name is missing', async () => {
+        const customerRepository = MockRepository();
+        const usecase = new CreateCustomerUseCase(customerRepository);
+
+        const inputWithoutName = {
+            ...input,
+            name: '',
+        };
+
+        await expect(usecase.execute(inputWithoutName)).rejects.toThrow('Name is required');
+    });
+});
